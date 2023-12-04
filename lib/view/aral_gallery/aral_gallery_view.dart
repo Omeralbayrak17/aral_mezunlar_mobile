da61b2ff7b56c24151f6232a1d48dc9daaa58573
OmerAlbayrak17 <omeralbayrak76@gmail.com>
2023-12-04 10:01:27 +0300

    aral galeri dinamik olarak verileri once cloud firestore sonra cloud storagedan cekiyor artik

--- lib/view/aral_gallery/aral_gallery_view.dart
+++ lib/view/aral_gallery/aral_gallery_view.dart
@@ -1,6 +1,11 @@
 import 'package:aral_mezunlar_mobile/controller/firebase_storage_controller.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../../extension/navigator_extension.dart';
 
 class AralGalleryView extends StatefulWidget {
   const AralGalleryView({super.key});
@@ -19,20 +24,23 @@ class _AralGalleryViewState extends State<AralGalleryView> {
       builder: (context, child) {
         return Scaffold(
           extendBody: true,
-          body: StreamBuilder<List<String>>(
-            stream: FirebaseStorageController.getAllDownloadUrls("aralgaleri").asStream(),
+          body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('aralgaleri').snapshots(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(child: CircularProgressIndicator());
               } else if (snapshot.hasError) {
                 return Text('Hata: ${snapshot.error}');
               } else {
-                List<String>? downloadUrls = snapshot.data;
+                List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+                documents.sort((a, b) => a['timestamp'].compareTo(b['timestamp']));
 
-                return (downloadUrls != null && downloadUrls.isNotEmpty)
-                    ? ListView.builder(
-                  itemCount: downloadUrls.length,
+                return ListView.builder(
+                  itemCount: documents.length,
                   itemBuilder: (context, index) {
+                    String description = documents[index]['description'];
+                    String documentId = documents[index].id;
+
                     return Padding(
                       padding: EdgeInsets.only(bottom: 20.h, left: 10.w, right: 10.w, top: 5.h),
                       child: Card(
@@ -40,24 +48,75 @@ class _AralGalleryViewState extends State<AralGalleryView> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(8.0),
                         ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            downloadUrls[index],
-                            fit: BoxFit.cover,
-                            height: 150.h,
-                            width: double.infinity,
-                          ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8)),
+                              child: FutureBuilder<String>(
+                                future: FirebaseStorageController.downloadImageFromStorage("aralgaleri", documentId),
+                                builder: (context, snapshot) {
+                                  if (snapshot.connectionState == ConnectionState.done) {
+                                    String userProfilePhotoUrl = snapshot.data ?? '';
+                                    return InkWell(
+                                      onTap: () {
+                                        Navigator.push(context, NavigatorExtension.expandFromMiddleAnimation(const AralGalleryView()));
+                                      },
+                                      splashColor: Colors.blue,
+                                      hoverColor: Colors.purpleAccent,
+                                      borderRadius: BorderRadius.circular(10),
+                                      child: CachedNetworkImage(
+                                        imageUrl: userProfilePhotoUrl,
+                                        placeholder: (context, url) => Shimmer.fromColors(
+                                          baseColor: Colors.grey[300]!,
+                                          highlightColor: Colors.grey[100]!,
+                                          child: SizedBox(
+                                            height: 130.h,
+                                          ),
+                                        ),
+                                        errorWidget: (context, url, error) => Shimmer.fromColors(
+                                          baseColor: Colors.grey[300]!,
+                                          highlightColor: Colors.grey[100]!,
+                                          child: SizedBox(
+                                            height: 130.h,
+                                          ),
+                                        ),
+                                        imageBuilder: (context, imageProvider) => Image(
+                                          height: 130.h,
+                                          width: double.infinity,
+                                          image: imageProvider,
+                                          fit: BoxFit.fill,
+                                        ),
+                                      ),
+                                    );
+                                  } else {
+                                    return Shimmer.fromColors(
+                                      baseColor: Colors.grey[300]!,
+                                      highlightColor: Colors.grey[100]!,
+                                      child: SizedBox(
+                                        height: 120.h,
+                                      ),
+                                    );
+                                  }
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                description,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     );
                   },
-                )
-                    : const Text('Gösterilecek dosya yok.');
+                );
               }
             },
           ),
-          floatingActionButton: FloatingActionButton(
+            floatingActionButton: FloatingActionButton(
             onPressed: (){
             },
             child: const Icon(Icons.add_a_photo),
