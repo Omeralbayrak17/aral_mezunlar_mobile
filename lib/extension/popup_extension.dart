dce95c9b3c0cfef65c620f72b4b0b085457758bd
OmerAlbayrak17 <omeralbayrak76@gmail.com>
2023-12-06 10:22:15 +0300

    Aral Galeri'de fotoğraflari silmek icin onayliyor musunuz popup eklendi

--- lib/extension/popup_extension.dart
+++ lib/extension/popup_extension.dart
@@ -1,7 +1,9 @@
 import 'package:aral_mezunlar_mobile/controller/firebase_auth_controller.dart';
 import 'package:aral_mezunlar_mobile/controller/firebase_firestore_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../controller/firebase_storage_controller.dart';
 import 'flushbar_extension.dart';
 
 class PopUpExtension{
@@ -33,6 +35,43 @@ class PopUpExtension{
       },
     );
   }
+
+  static void showGalleryImageDeleteConfirmationDialog(BuildContext context, String documentId) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Fotoğrafı Sil'),
+          content: const Text('Bu fotoğrafı silmek istiyor musunuz?'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  FirebaseFirestore.instance.collection('aralgaleri').doc(documentId).delete();
+
+                  FirebaseStorageController.deleteImageFromStorage("aralgaleri", documentId);
+
+                  print('Belge ve dosya başarıyla silindi.');
+                } catch (e) {
+                  print('Hata oluştu: $e');
+                }
+                Navigator.of(context).pop();
+                if(context.mounted) FlushbarExtension.oneMessageFlushbar(context, "Etkinlik silindi");
+              },
+              child: const Text('Evet'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Hayır'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 
   static void showForgetPasswordDialog(BuildContext context) {
     TextEditingController emailController = TextEditingController();
