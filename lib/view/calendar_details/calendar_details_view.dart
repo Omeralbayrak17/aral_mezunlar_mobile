c0ff7fb467695d8b2a12fb33571a26e11f73b237
OmerAlbayrak17 <omeralbayrak76@gmail.com>
2023-12-11 16:31:44 +0300

    Some changes about app name and gradle

--- lib/view/calendar_details/calendar_details_view.dart
+++ lib/view/calendar_details/calendar_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:whatsapp_unilink/whatsapp_unilink.dart';
 
